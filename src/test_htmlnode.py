a5c2cfd7621a162ec9fcc62399c8e505ea51268a
jess-armstrong <jessfarmstrong@gmail.com>
2025-07-21 15:38:03 -0400

    add leafnode child class to represent html tags with no children

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_print(self):
@@ -39,3 +39,18 @@ class TestHTMLNode(unittest.TestCase):
             node.props_to_html(),
             ' property1="test property" property2="test2"'
         )
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a_props(self):
+        node = LeafNode("a", "Hello, world!", {"href": "https://www.amazon.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.amazon.com">Hello, world!</a>')
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+if __name__ == "__main__":
+    unittest.main()
